c4ccd70a5b222fd0962175dd200e9f68be57db8d
Aakash Jha <a25jha@gmail.com>
2020-07-15 02:00:06 +0530

    Learning typeScript-null,undefined,using Datatypes

--- main.js
+++ main.js
@@ -2,3 +2,17 @@
 exports.__esModule = true;
 var message = "Welcome Back";
 console.log(message);
+var x = 100;
+var y;
+var x1 = 100;
+//const y1; Can't because const has to initilaized at always.
+var isBeginner = true;
+//put a ':' and datatype to define the cariable datatype
+var total = 0;
+var name = "aakash";
+//Template strings
+var sentence = "My name is " + name + "\nI am a beginner in TypeScript";
+console.log(sentence);
+var n = null; //subtye of all other types
+var u = undefined; //subtype of all other types
+//Read difference between Null and Undefined
--- main.ts
+++ main.ts
@@ -1,3 +1,25 @@
 export {};
 let message = "Welcome Back";
 console.log(message);
+
+let x = 100;
+let y;
+const x1 = 100;
+//const y1; Can't because const has to initilaized at always.
+
+let isBeginner: boolean = true;
+//put a ':' and datatype to define the cariable datatype
+
+let total: number = 0;
+let name: string = "aakash";
+
+//Template strings
+let sentence: string = `My name is ${name}
+I am a beginner in TypeScript`;
+
+console.log(sentence);
+
+let n: null = null; //subtye of all other types
+let u: undefined = undefined; //subtype of all other types
+
+//Read difference between Null and Undefined
